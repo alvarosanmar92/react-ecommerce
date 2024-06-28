9fb4816d6f09986229abcd586f984677c8204898
alvarosanmar92 <alvaro.sanmar92@gmail.com>
2024-06-28 18:15:39 +0200

    Ultimos retoques

---
+++ codercommerce/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Descriptionbox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className='descriptionbox-navigator'>
+            <div className="descriptionbox-nav-box">Descripción</div>
+            <div className="descriptionbox-nav-box face">Reseñas (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Podríamos definir un ecommerce como una tienda virtual. Un método de compraventa que utiliza internet como medio para realizar transacciones y contactar con sus consumidores. No sólo mediante una página web, sino también a través de las redes sociales.</p>
+            <p>Podríamos definir un ecommerce como una tienda virtual. Un método de compraventa que utiliza internet como medio para realizar transacciones y contactar con sus consumidores. No sólo mediante una página web, sino también a través de las redes sociales.</p>
+        </div>
+    </div>
+  )
+}
+
+
+export default DescriptionBox
